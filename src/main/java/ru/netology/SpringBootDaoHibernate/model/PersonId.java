58941faf527a137d328fb0e9c2545311e9f4156f
Eva <nebreoma12@yandex.ru>
2025-03-27 22:54:44 +0300

    Change PersonId.java and README.md in branch jpa-repository

--- src/main/java/ru/netology/SpringBootDaoHibernate/model/PersonId.java
+++ src/main/java/ru/netology/SpringBootDaoHibernate/model/PersonId.java
@@ -4,9 +4,9 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class PersonId implements Serializable {
-    private String name;
-    private String surname;
-    private int age;
+    protected String name;
+    protected String surname;
+    protected int age;
 
     public PersonId() {
     }
